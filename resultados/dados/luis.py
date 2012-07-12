6144323ea1d0d614fe5229dd5db751a8456fc886
abelardo <abbbekgb@gmail.com>
2012-07-12 12:00:44 -0400

    predicao

--- resultados/dados/luis.py
+++ resultados/dados/luis.py
@@ -2,24 +2,25 @@ from resultados.models import Resultado
 import time
 import random
 
-
-def super_metodo(TIME_MAX):
+def grupo(milhar):
+    return (((int(milhar)-1)%100)/4)+1
+        
+def constroi_cache(res):
+    grupos_cache = []
+    for resultado in res:
+        grupos_cache.append([grupo(milhar) for milhar in resultado.resultados()])
+    return grupos_cache
+        
+        
+def metodo(TIME_MAX):
     time_inicial = time.time()
     max_errors = 0
+    
+    total = 0
 
     # inicio com 40
-    resultados = Resultado.objects.order_by('-data')[:60]
-
-    def grupo(milhar):
-        return (((int(milhar)-1)%100)/4)+1
-        
-    grupos_cache =  []
-
-    def constroi_cache(res):
-        for resultado in res:
-            grupos_cache.append([grupo(milhar) for milhar in resultado.resultados()])
-
-    constroi_cache(resultados)
+    resultados = Resultado.objects.order_by('-data')[:60]        
+    grupos_cache = constroi_cache(resultados)
     while time.time() - time_inicial < TIME_MAX:
         meus_randons = []
         wrong_random = False
@@ -30,9 +31,10 @@ def super_metodo(TIME_MAX):
             if time.time() - time_inicial >= TIME_MAX:
                 break
             # gero random
-            random_grupo = random.randint(0,24)           
+            random_grupo = random.randint(1,25)           
             # verifico se o grupo randomizado saiu no resultado
             wrong_random = True  
+            total += 1
             for premio in grupos_cache[position]:
                 # achei uma ocorrencia do random nos premios
                 if premio == random_grupo:
@@ -45,7 +47,25 @@ def super_metodo(TIME_MAX):
         if position > max_errors:
             max_randons = meus_randons
         max_errors = max([max_errors, position]) 
-    print("randons :", max_randons)
-    print("Maximo de erros foi:", max_errors)   
+    print("randons :", max_randons, "total :", total)
+    print("Maximo de erros foi:", max_errors) 
+    
+def predicao(num_erros):
+    ini = time.time()
+    resultados = Resultado.objects.order_by('-data')[:num_erros]
+    cache = constroi_cache(resultados)
+    all_errors = False
+    while not all_errors:
+      # tenta errar num_erros
+      all_errors = True
+      for resultado in cache[::-1]:
+          random_grupo = random.randint(1,25)
+          if random_grupo not in resultado:
+              all_errors = False
+              break
+    print("total :", time.time() - ini)
+    return [random.randint(1,25) for x in range(3)]
+            
+                        
         
         
